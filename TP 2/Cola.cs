a35e3aee35451b678a78bba8ec797c53e52fb604
Braian <braianalvarez342@gmail.com>
2023-10-03 18:31:21 -0300

    commit inicial

---
+++ TP 2/Cola.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Braian
+ * Date: 5/9/2023
+ * Time: 20:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TP_2
+{
+	/// <summary>
+	/// Description of Cola.
+	/// </summary>
+	public class Cola<T>
+	{
+		private List<T> datos = new List<T>();
+		
+		public void encolar(T elem)
+		{	
+			this.datos.Add(elem);
+		}
+		
+		public T desencolar()
+		{	
+			T temp = this.datos[0];
+			this.datos.RemoveAt(0);
+			return temp;
+		}
+		
+		public T tope()
+		{	
+			return this.datos[0];
+		}
+		
+		public bool esVacia()
+		{	
+			return this.datos.Count == 0;
+		}
+		
+		
+		
+	}
+}
---
+++ TP 2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Braian
+ * Date: 3/10/2023
+ * Time: 17:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TP_2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			ArbolGeneral<int> arbol =  new ArbolGeneral<int>(10);
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
